11feca1016eb00f00c3743df2ea1deae515371fa
aydentownsley <aydentownsley@gmail.com>
2021-04-03 20:08:52 +0000

    new methond

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
+"100-puppet_ssh_config.pp" 15L, 272C                                                     1,1           All
 #!/usr/bin/puppet
 #Setting up serv to connect w/o pwd
 
-class { '::ssh::server':
-  options => {
-    'IdentityFile'           => '~/.ssh/holberton',
-    'PasswordAuthentication' => 'no',
-  },
+augeas { 'id file':
+  context => '/etc/ssh/ssh_config',
+  changes => 'set IdentityFile ~/.ssh/holberton',
 }
+
+augeas { 'pwd auth':
+  context => '/etc/ssh/ssh_config',
+  changes => 'set PasswordAuthentication no',
+}
+
