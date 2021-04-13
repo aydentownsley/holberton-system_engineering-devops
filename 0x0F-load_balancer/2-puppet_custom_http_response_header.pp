bfd421993fef5aeb46e0eef1644c41f8328c22d6
aydentownsley <aydentownsley@gmail.com>
2021-04-13 02:54:21 +0000

    puppet lint

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,14 @@
 # adding custom response header
 package { 'nginx':
-	ensure => installed
+  ensure => installed
 }
 
 file_line {
-	command => 'sed -i "s/http {/http {\n\tadd_header X-Served-By \$hostname;\n/"' /etc/nginx/nginx.conf
+  command => 'sed -i "s/http {/http {\n\tadd_header X-Served-By \$hostname;\n/"' /etc/nginx/nginx.conf
 }
 
 service { 'nginx':
-	ensure  => running,
-	restart => true,
-	require => Package['nginx'],
+  ensure  => running,
+  restart => true,
+  require => Package['nginx'],
 }
