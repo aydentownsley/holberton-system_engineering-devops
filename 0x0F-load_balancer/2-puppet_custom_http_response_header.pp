40a3366988684259bbf72ec5ed40fddd116335c2
aydentownsley <aydentownsley@gmail.com>
2021-04-13 03:04:57 +0000

    adding 2 with sed delim replacement

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,8 +3,8 @@ package { 'nginx':
   ensure => installed
 }
 
-exec { 'add header':
-  command => "sed -i 's/http {/http {\n\tadd_header X-Served-By \$hostname;\n/' /etc/nginx/nginx.conf",
+exec { 'addheader':
+  command => "sed -i 's|http {|http {\n\tadd_header X-Served-By \$hostname;\n|' /etc/nginx/nginx.conf",
 }
 
 service { 'nginx':
