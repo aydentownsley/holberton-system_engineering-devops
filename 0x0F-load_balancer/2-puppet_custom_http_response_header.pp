838b33393c73883087f06ebb3507ac9cf7cb42a3
aydentownsley <aydentownsley@gmail.com>
2021-04-13 16:06:42 +0000

    adding new script test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,8 +4,10 @@ package { 'nginx':
   ensure => installed,
 }
 
-exec { 'addheader':
-  command => "sed -i 's\http {\http {\\n\\tadd_header X-Served-By \$hostname;\\n\' /etc/nginx/nginx.conf",
+file_line { 'add_header':
+  path  => '/etc/nginx/nginx.conf',
+  line  => 'http {  add_header X-Served-By $hostname;',
+  match => 'http {',
 }
 
 service { 'nginx':
