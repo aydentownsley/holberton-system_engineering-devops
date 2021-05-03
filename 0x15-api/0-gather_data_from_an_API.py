db4a197b42be66e41bc1d2265fd7f0332b064a61
aydentownsley <aydentownsley@gmail.com>
2021-05-03 20:42:14 +0000

    adding 0 with main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,30 +3,34 @@ import json
 import sys
 from urllib import request
 
-tasks = 0
-tasks_comp = 0
-req = request.Request('https://jsonplaceholder.typicode.com/todos')
-req_user = request.Request('https://jsonplaceholder.typicode.com/users')
 
-with request.urlopen(req) as resp:
-    data = json.loads(resp.read().decode('utf-8'))
-    for x in data:
-        if x['userId'] == int(sys.argv[1]):
-            tasks += 1
-            if x['completed'] is True:
-                tasks_comp += 1
+if __name__ == "__main__":
 
-with request.urlopen(req_user) as resu:
-    data = json.loads(resu.read().decode('utf-8'))
-    for k in data:
-        if k['id'] == int(sys.argv[1]):
-            name = k['name']
+    tasks = 0
+    tasks_comp = 0
+    req = request.Request('https://jsonplaceholder.typicode.com/todos')
+    req_user = request.Request('https://jsonplaceholder.typicode.com/users')
 
-print('Employee {} is done with tasks({}/{}):'.format(name, tasks_comp, tasks))
+    with request.urlopen(req) as resp:
+        data = json.loads(resp.read().decode('utf-8'))
+        for x in data:
+            if x['userId'] == int(sys.argv[1]):
+                tasks += 1
+                if x['completed'] is True:
+                    tasks_comp += 1
 
-with request.urlopen(req) as resp:
-    data = json.loads(resp.read().decode('utf-8'))
-    for x in data:
-        if x['userId'] == int(sys.argv[1]):
-            if x['completed'] is True:
-                print('\t {}'.format(x['title']))
+    with request.urlopen(req_user) as resu:
+        data = json.loads(resu.read().decode('utf-8'))
+        for k in data:
+            if k['id'] == int(sys.argv[1]):
+                name = k['name']
+
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+          tasks_comp, tasks))
+
+    with request.urlopen(req) as resp:
+        data = json.loads(resp.read().decode('utf-8'))
+        for x in data:
+            if x['userId'] == int(sys.argv[1]):
+                if x['completed'] is True:
+                    print('\t {}'.format(x['title']))
