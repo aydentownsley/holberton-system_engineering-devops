81c4c349ddf334e2cf431ef27bf2c21b8a389902
aydentownsley <aydentownsley@gmail.com>
2021-05-19 21:53:23 +0000

    adding 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" get top ten titles from hot """
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ top ten titles """
+    headers = {'User-Agent': 'test'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    sub_res = requests.get(url, headers=headers)
+    if sub_res.status_code == 200:
+        hot = sub_res.json().get('data').get('children')
+        for el in hot:
+            print(el.get('data').get('title'))
+
+    else:
+        return (0)
